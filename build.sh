c414ef967852d0c70bb566844196876059ed0c49
Lukas Rytz <lukas.rytz@gmail.com>
2019-09-11 12:41:30 +0200

    try something

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+verPat="[0-9]+\.[0-9]+\.[0-9]+(-[A-Za-z0-9-]+)?"
+tagPat="^v$verPat(#$verPat#[0-9]+)?$"
+
+if [[ "$RELEASE" == "true" && "$TRAVIS_TAG" =~ $tagPat ]]; then
+  releaseTask="ci-release"
+
+  tagScalaVer=$(echo $TRAVIS_TAG | sed s/[^#]*// | sed s/^#//)
+  if [[ "tagScalaVer" != "" ]]; then
+    if [[ "$TRAVIS_JOB_NUMBER" =~ "[0-9]+\.1" ]]; then
+      setTagScalaVersion='set every scalaVersion := "'$tagScalaVer'"'
+    else
+      echo "The release for Scala $tagScalaVer is built by the job in the travis job matrix"
+      exit 0
+    fi
+  fi
+fi
+
+sbt "$setTagScalaVersion" publishLocal "$releaseTask"
